12e05b025aac242e869eac5a853c1840ab5f3aab
prog223 <naneayvazyan4537@gmail.com>
2024-03-14 16:49:32 +0400

    Connect server to db

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 dotenv.config();
+
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch((err) => console.log(err));
 
 const app = express();
 app.use(express.json());
